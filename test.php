c699715a46c000a07d6bdb952a1927821d0119bd
iramgee <garciairam@gmail.com>
2015-08-25 11:43:02 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+ $purl = $_GET['purl'];
+?>
+go to your purl, <a href="<?php echo $purl.'.testmaml.m.mdl.io/tjmaxx'; ?>" target="_blank">here</a><br>
+
+<?php
+
+print_r($_GET);
+?>
+<br>
+<?php
+echo $purl;
+?>
+
+</body>
+</html>
